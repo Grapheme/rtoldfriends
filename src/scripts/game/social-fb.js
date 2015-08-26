35e9ec228d7061f82bc73a5c410d563e01e58395
Igor Alekseev <mail@igoralekseev.info>
2015-08-26 17:00:53 +0300

    тестируем фейсбук

--- src/scripts/game/social-fb.js
+++ src/scripts/game/social-fb.js
@@ -1,5 +1,38 @@
 if (!Game.social) Game.social = {};
 
+$FB = {
+  init: function (options) {
+    return deferred(function (resolve) {
+      $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+        var settings = {
+          appId: options.appId,
+          version: 'v2.3'
+        };
+
+        FB.init(settings);
+        FB.getLoginStatus(function(response) {
+          if (response.status == 'connected') {
+            resolve(response);
+          } else {
+            var fb_perms = {scope: 'publish_actions, public_profile, user_birthday, user_photos, user_likes, user_friends'};
+            FB.login(function(response) {
+              resolve(response);
+            }, fb_perms);
+          }
+        });
+      });
+    });
+  },
+
+  api: function(path, method, params) {
+    if (!params) params = {};
+    return deferred(function(resolve) {
+      FB.api(path, method, params, resolve);
+    });
+  }
+};
+
+
 Game.social.FB = {
   appId: 1637810779822904,
   getQuestions: function(callback) {
@@ -7,36 +40,38 @@ Game.social.FB = {
     callback(questionsArray);
   },
   init: function() {
-    var t = this;
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-      var settings = {
-        appId: t.appId,
-        version: 'v2.3'
-      };
-      FB.init(settings);
-      FB.getLoginStatus(function(response) {
-        if (response.status == 'connected') {
-          t.start(response);
-        } else {
-          var fb_perms = {scope: 'publish_actions, public_profile, user_birthday, user_photos, user_likes, user_friends'};
-          FB.login(function(response) {
-            t.start(response);
-          }, fb_perms);
-        }
+
+    $FB.init({ appId: this.appId }).then(function(data) {
+      // FB.api('/me', function(response) {
+      //   if(response) {
+      //     $('.js-your-name').text(response.name.split(' ')[0]);
+      //   }
+      // });
+
+      // $FB.api('/me/taggable_friends', 'get').then(function(response) {
+      //   console.log('friends', response)
+      // });
+
+      console.log('sdsd',data );
+
+      // $.getJSON('http://localhost:4567/' + data.authResponse.userID).then(function(data) {
+        // console.log('friends?', data)
+      // });
+
+      $.getJSON('https://graph.facebook.com/' +  data.authResponse.userID).then(function(data) {
+        console.log('dd', data);
+      })
+
+      $FB.api('/me').then(function(data) {
+        console.log('sasd', data);
+      });
+
+
+      Game.preloader.show('http://graph.facebook.com/' + data.authResponse.userID + '/picture?width=160&height=160');
+      t.getQuestions(function(questionsArray){
+        Game.questions.init(questionsArray);
+        Game.preloader.close();
       });
     });
-  },
-  start: function(data) {
-    var t = this;
-    Game.preloader.show('http://graph.facebook.com/' + data.authResponse.userID + '/picture?width=160&height=160');
-    t.getQuestions(function(questionsArray){
-      Game.questions.init(questionsArray);
-      Game.preloader.close();
-    });
-    FB.api('/me', function(response) {
-      if(response) {
-        $('.js-your-name').text(response.name.split(' ')[0]);
-      }
-    });
-  }  
+  }
 };
