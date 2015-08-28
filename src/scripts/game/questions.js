144334076c596a7f424f027f04b6fe62bd36d8d4
Igor Alekseev <mail@igoralekseev.info>
2015-08-28 19:02:42 +0300

    картинки в сафари

--- src/scripts/game/questions.js
+++ src/scripts/game/questions.js
@@ -25,7 +25,10 @@ Game.questions = {
         if (question.type == 'text') {
           $(this).find('.js-qaImage div').text(answer.title);
         } else if (question.type == 'people') {
-          $(this).find('.js-qaImage').css('background-image', 'url(' + answer.image || '' + ')'); 
+          // $(this).find('.js-qaImage div').text(answer.title);
+
+          console.log('dsds', 'url(' + answer.image || '' + ')');
+          $(this).find('.js-qaImage').css('background-image', 'url(' + (answer.image || '') + ')'); 
           $(this).find('.js-qaTitle').text(answer.title);
         }
       });
